a3807f06f00259926910ac01cf4c206534c5d390
Fkhrayef <alkhrayefff@gmail.com>
2025-09-07 11:45:08 +0300

    add unit test class for CarTransferRequestService and configure JUnit 5 dependencies in pom.xml

---
+++ src/test/java/com/fkhrayef/motor/CarTransferRequestServiceTest.java
@@ -0,0 +1,15 @@
+package com.fkhrayef.motor;
+
+import com.fkhrayef.motor.Service.CarTransferRequestService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CarTransferRequestServiceTest {
+
+    @InjectMocks
+    CarTransferRequestService carTransferRequestService;
+
+
+}
